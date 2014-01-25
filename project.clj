251e9fd2cdec998a3167025ddf284ae5c4cab6c4
Roman Scherer <roman.scherer@burningswell.com>
2014-01-25 17:09:01 +0100

    Changed group name.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-spark/clj-spark "0.1.0-SNAPSHOT"
+(defproject org.clojars.r0man/clj-spark "0.1.0-SNAPSHOT"
   :min-lein-version "2.0.0"
   :license {:name "Eclipse Public License" :url "http://www.eclipse.org/legal/epl-v10.html"}
   :description "Clojure API wrapper on the Spark project (http://spark-project.org/)"
