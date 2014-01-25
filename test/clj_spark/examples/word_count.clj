9adadbd5fcb2f3bb04da6cd80a48434477e59786
Roman Scherer <roman.scherer@burningswell.com>
2014-01-25 17:06:50 +0100

    Added word count example.

---
+++ test/clj_spark/examples/word_count.clj
@@ -0,0 +1,19 @@
+(ns clj-spark.examples.word-count
+  (:refer-clojure :exclude [fn])
+  (:require [clojure.string :refer [split]]
+            [clojure.test :refer :all]
+            [clj-spark.api :as k]
+            [serializable.fn :refer [fn]]))
+
+(defn word-count [context file]
+  (-> (.textFile context file)
+      (k/flat-map (fn [x] (split x #"\s+")))
+      (k/map (fn [word] [word 1]))
+      (k/reduce-by-key +)
+      (.collect)
+      (->> (into []))))
+
+(deftest test-word-count
+  (k/with-context [context "local" "test-word-count"]
+    (is (= [["" 27] ["AND" 3] ["making" 1] ["or," 1] ["places:" 1]]
+           (take 5 (word-count context "LICENSE"))))))
