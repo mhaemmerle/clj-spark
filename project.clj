cebbc0199185f4c6639028ec9f5a9389aa8e3c0b
Roman Scherer <roman.scherer@burningswell.com>
2014-01-27 19:08:06 +0100

    Use provided profile and Clojure 1.5.1.

--- project.clj
+++ project.clj
@@ -4,15 +4,16 @@
   :description "Clojure API wrapper on the Spark project (http://spark-project.org/)"
   :url "https://github.com/TheClimateCorporation/clj-spark"
   :jvm-opts ["-Dlog4j.configuration=file:log4j.properties"]
-  :dependencies [[org.clojure/clojure "1.5.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.logging "0.2.6"]
                  [org.clojure/tools.cli "0.3.1"]
                  [org.clojars.mlimotte/serializable-fn "0.0.3"]]
-                                        ; Include te Spark dep for compile and run, but not for uberjar
-  :profiles {:dev {:dependencies [[org.apache.hadoop/hadoop-client "1.0.4"]
-                                  [org.apache.spark/spark-core_2.9.3 "0.8.1-incubating"]]}}
+  :profiles {:provided {:dependencies [[org.apache.hadoop/hadoop-client "1.0.4"]
+                                       [org.apache.spark/spark-core_2.9.3 "0.8.1-incubating"]]}}
+  :repositories {"Akka repository" "http://repo.akka.io/releases"}
   :aot [clj-spark.spark.functions
         clj-spark.api
         clj-spark.util
-        clj-spark.examples.query]
+        clj-spark.examples.query
+        clj-spark.examples.word-count]
   :main clj-spark.examples.query)
