6f32594d2a1c534e05042c438167889a2ba2aa78
Roman Scherer <roman.scherer@burningswell.com>
2014-02-03 10:33:13 +0100

    Moved warn-on-reflection into global vars.

--- project.clj
+++ project.clj
@@ -16,5 +16,4 @@
         clj-spark.util
         clj-spark.examples.query
         clj-spark.examples.word-count]
-  :main clj-spark.examples.query
-  :warn-on-reflection true)
+  :global-vars {*warn-on-reflection* true})
