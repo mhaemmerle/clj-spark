9cb4701f9c6d7e72fa288e8789e85eb4cd58a585
Roman Scherer <roman.scherer@burningswell.com>
2014-02-03 11:05:49 +0100

    Use JavaRDDLike signature where possible.

--- src/clj_spark/api.clj
+++ src/clj_spark/api.clj
@@ -7,7 +7,7 @@
   (:import java.util.Comparator
            scala.Tuple2
            [clj_spark.fn Function Function2 FlatMapFunction PairFunction VoidFunction]
-           [org.apache.spark.api.java JavaSparkContext JavaRDD]))
+           [org.apache.spark.api.java JavaSparkContext JavaRDD JavaRDDLike JavaPairRDD]))
 
 (defn spark-context
   [& {:keys [master job-name spark-home jars environment]}]
@@ -67,8 +67,6 @@
   (fn [coll]
     (clojure.core/map (fn [f x] (f x)) fs coll)))
 
-                                        ; RDD construction
-
 (defn text-file
   [^JavaSparkContext context filename]
   (.textFile context filename))
@@ -77,66 +75,50 @@
   [^JavaSparkContext context lst]
   (.parallelize context lst))
 
-                                        ; Transformations
-
-(defn echo-types
-                                        ; TODO make this recursive
-  [c]
-  (if (coll? c)
-    (println "TYPES" (clojure.core/map type c))
-    (println "TYPES" (type c)))
-  c)
-
-(defn trace
-  [msg]
-  (fn [x]
-    (prn "TRACE" msg x)
-    x))
-
 (defn map
   "Return a new RDD by applying a function to all elements of this RDD."
-  [^JavaRDD rdd f]
+  [^JavaRDDLike rdd f]
   (.map rdd (Function. f)))
 
 (defn reduce
   "Reduces the elements of this RDD using the specified commutative
   and associative binary operator."
-  [^JavaRDD rdd f]
+  [^JavaRDDLike rdd f]
   (.reduce rdd (Function2. f)))
 
 (defn flat-map
   "Return a new RDD by first applying a function to all elements of
   this RDD, and then flattening the results."
-  [^JavaRDD rdd f]
+  [^JavaRDDLike rdd f]
   (.flatMap rdd (FlatMapFunction. f)))
 
 (defn filter
   "Return a new RDD containing only the elements that satisfy a predicate."
-  [^JavaRDD rdd f]
+  [rdd f]
   (.filter rdd (Function. (ftruthy? f))))
 
 (defn foreach
   "Applies a function f to all elements of this RDD."
-  [^JavaRDD rdd f]
+  [^JavaRDDLike rdd f]
   (.foreach rdd (VoidFunction. f)))
 
 (defn aggregate
   "Aggregate the elements of each partition, and then the results for
   all the partitions, using given combine functions and a neutral zero
   value."
-  [^JavaRDD rdd zero-value seq-op comb-op]
+  [^JavaRDDLike rdd zero-value seq-op comb-op]
   (.aggregate rdd zero-value (Function2. seq-op) (Function2. comb-op)))
 
 (defn fold
   "Aggregate the elements of each partition, and then the results for
   all the partitions, using a given associative function and a neutral
   zero value."
-  [^JavaRDD rdd zero-value f]
+  [^JavaRDDLike rdd zero-value f]
   (.fold rdd zero-value (Function2. f)))
 
 (defn reduce-by-key
   "Merge the values for each key using an associative reduce function."
-  [^JavaRDD rdd f]
+  [^JavaRDDLike rdd f]
   (-> rdd
       (.map (PairFunction. identity))
       (.reduceByKey (Function2. f))
@@ -144,22 +126,22 @@
 
 (defn group-by-key
   "Return an RDD of grouped elements."
-  [^JavaRDD rdd]
+  [^JavaRDDLike rdd]
   (-> rdd
       (.map (PairFunction. identity))
       .groupByKey
       (.map (Function. untuple))))
 
 (defn sort-by-key
-  ([^JavaRDD rdd]
+  ([^JavaRDDLike rdd]
      (sort-by-key rdd compare true))
-  ([^JavaRDD rdd x]
+  ([^JavaRDDLike rdd x]
      ;; Note: RDD has a .sortByKey signature with just a Boolean arg,
      ;; but it doesn't seem to work when I try it, bool is ignored.
      (if (instance? Boolean x)
        (sort-by-key rdd compare x)
        (sort-by-key rdd x true)))
-  ([^JavaRDD rdd compare-fn asc?]
+  ([^JavaRDDLike rdd compare-fn asc?]
      (-> rdd
          (.map (PairFunction. identity))
          (.sortByKey
@@ -172,7 +154,7 @@
 (defn join
   "Return an RDD containing all pairs of elements with matching keys
   in this and other."
-  [^JavaRDD rdd ^JavaRDD other]
+  [^JavaRDDLike rdd ^JavaRDD other]
   (-> rdd
       (.map (PairFunction. identity))
       (.join (.map other (PairFunction. identity)))
@@ -180,38 +162,38 @@
 
 (defn cache
   "Persist this RDD with the default storage level (MEMORY_ONLY)."
-  [^JavaRDD rdd]
+  [rdd]
   (.cache rdd))
 
 (defn collect
   "Return a seq that contains all of the elements in this RDD."
-  [^JavaRDD rdd]
+  [^JavaRDDLike rdd]
   (.collect rdd))
 
 (defn count
   "Return the number of elements in the RDD."
-  [^JavaRDD rdd]
+  [^JavaRDDLike rdd]
   (.count rdd))
 
 (defn distinct
   "Return a new RDD containing the distinct elements in this RDD."
-  ([^JavaRDD rdd]
+  ([rdd]
      (.distinct rdd))
-  ([^JavaRDD rdd partitions]
+  ([rdd partitions]
      (.distinct rdd partitions)))
 
 (defn first
   "Return the first element in this RDD."
-  [^JavaRDD rdd]
+  [^JavaRDDLike rdd]
   (.first rdd))
 
 (defn glom
   "Return an RDD created by coalescing all elements within each
 partition into an array."
-  [^JavaRDD rdd]
+  [^JavaRDDLike rdd]
   (.glom rdd))
 
 (defn take
   "Take the first num elements of the RDD."
-  [^JavaRDD rdd cnt]
+  [^JavaRDDLike rdd cnt]
   (.take rdd cnt))
